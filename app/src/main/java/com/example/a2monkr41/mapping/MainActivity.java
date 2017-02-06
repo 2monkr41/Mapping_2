dc002372901d6875e0ce018beaaddbf6f788da95
2monkr41 <2monkr41>
2017-02-06 17:01:05 +0000

    Initial commit

---
+++ app/src/main/java/com/example/a2monkr41/mapping/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.a2monkr41.mapping;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.content.Intent;
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return false;
+    }
+}
